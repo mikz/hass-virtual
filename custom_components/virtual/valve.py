7bf596f9b034f5dce23f49dbab427ce73e6af291
Michal Cichra <mikz@users.noreply.github.com>
2025-07-22 22:09:12 +0200

    rework VirtualOpenableEntity set_position
    * allows for instant changes when duration is set to 0
    * allows floats as ticks for valves and covers
    * prevents overshooting the position
    * handles interrupts

--- custom_components/virtual/valve.py
+++ custom_components/virtual/valve.py
@@ -23,7 +23,7 @@ from . import get_entity_configs
 from .const import *
 from .entity import (
     VirtualOpenableEntity,
-    virtual_schema,
+    virtual_schema, positive_tick,
 )
 
 
@@ -36,12 +36,12 @@ DEFAULT_VALVE_VALUE = "open"
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(virtual_schema(DEFAULT_VALVE_VALUE, {
     vol.Optional(CONF_CLASS): cv.string,
     vol.Optional(CONF_OPEN_CLOSE_DURATION, default=10): cv.positive_int,
-    vol.Optional(CONF_OPEN_CLOSE_TICK, default=1): cv.positive_int,
+    vol.Optional(CONF_OPEN_CLOSE_TICK, default=1): positive_tick,
 }))
 VALVE_SCHEMA = vol.Schema(virtual_schema(DEFAULT_VALVE_VALUE, {
     vol.Optional(CONF_CLASS): cv.string,
     vol.Optional(CONF_OPEN_CLOSE_DURATION, default=10): cv.positive_int,
-    vol.Optional(CONF_OPEN_CLOSE_TICK, default=1): cv.positive_int,
+    vol.Optional(CONF_OPEN_CLOSE_TICK, default=1): cv.positive_tick,
 }))
 
 
